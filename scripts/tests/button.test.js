667ddc3c3a2d572ae03cbad8a656883fa75acb2d
Natalitta <nataliialebedieva@gmail.com>
2023-06-27 18:56:35 +0000

    Add mocking DOM

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -2,4 +2,16 @@
  * @jest-environment jsdom
  */
 
+const { TestScheduler } = require("jest");
 const buttonClick = require("../button");
+
+beforeEach ( () => {
+    document.body.innerHTML = "<p id='par'></p>";
+});
+
+describe("DOM test", () => {
+    test("expects p content to change", () => {
+        buttonClick();
+        expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
+    });
+});
--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -13,12 +13,7 @@ describe("Calculator", () => {
         test("should return 20 for 10 + 10", () => {
             expect(addition(10, 10)).toBe(20);
         });
-        test("should return error for " " + 10", () => {
-            expect(addition(" ", 10)).toBe(Error);
-        });
-        test("should return error for "a" + 10", () => {
-            expect(addition("a", 10)).toBe(error);
-        });
+        
     });
 
     describe("Subtraction function", () => {
